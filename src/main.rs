b11f9f6bce3e64127e5ec625474aa9e6b65b3717
lly <196633982@qq.com>
2023-06-28 00:00:28 +0800

    直接调用EnumProcessModulesEx 拒绝访问 需要考虑新方法

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,32 @@ use windows::{core::*,Win32::Foundation::*,
      };
  use core::ffi::{c_void,c_int,c_longlong,c_long};
 
-
-fn get_base_address(mut hwd: HANDLE ,flag:BOOL)->u32{
+ const MAX_PATH:u32 = 260;
+ const PROCESS_NAME :&str = "GenshinImpact.exe";
+ extern "system" fn get_base_address( hwd: HANDLE ,flag:bool)->u32{
     unsafe{
-        let mut  hmodule = HMODULE::default();
-        let mut cbNeeded = 0u32;
-        EnumProcessModules(  hwd,  hmodule,1024u32,cbNeeded);
-    
+        let mut hmodule: [HMODULE;1024]=[HMODULE::default();1024];
+        // let ressss = &mut  hmodule;
+        let mut cbNeeded: u32 = 0;
+        let eb = EnumProcessModulesEx(  hwd,  &mut  hmodule[0] ,1024,&mut  cbNeeded as *mut u32,LIST_MODULES_ALL);
+        if eb.as_bool(){
+            println!("EnumProcessModules调用成功");
+           
+            for i in 0.. cbNeeded{
+                let mut szModName_temp = [0u16;256];
+                let mut szModName =szModName_temp.as_mut_slice();
+                // println!("hmodule{:?}",i);
+                let rerr  = GetModuleFileNameExW(hwd,hmodule[i as usize],szModName);
+                println!("module {:?}",szModName);
+
+
+            }
+            // GetModuleFileNameExW
+        }else{
+            println!("EnumProcessModules调用失败 {:?}",GetLastError());
+        }
+        0
     }
-
-
-    0
 }
 
 
@@ -26,7 +41,7 @@ fn main() {
     unsafe {
         // EnumWindows(Some(enum_window), 0);
         println!("[>] entering  \n");
-        let thwnd = FindWindowW(None,w!("Sekiro"));
+        let thwnd = FindWindowW(None,w!("原神"));
         println!("window_handle：{:?}",thwnd);
         let mut  ptr:  u32  = 0     ;
         let rptr = &mut ptr;
@@ -37,12 +52,14 @@ fn main() {
         let basead: u64  = 0x143D5AAC0    ;
         // let rbuffer: u64 = 0  ;
         let mut ret: u64 = Default::default();
-
+        
        
         let s =process_handle.unwrap();
-        let a = ReadProcessMemory( s,  basead as *const c_void,  &mut ret as *mut u64 as *mut c_void, 8, None);
-        println!("a:{:?} rbuffer{:?} ",a, ret);
-        let   aim1 = ret+0x68;
+
+        let res2: u32 = get_base_address(s, false);
+        // let a = ReadProcessMemory( s,  basead as *const c_void,  &mut ret as *mut u64 as *mut c_void, 8, None);
+        // println!("a:{:?} rbuffer{:?} ",a, ret);
+        // let   aim1 = ret+0x68;
          
         // let b = ReadProcessMemory( s,  aim1 as *const c_void,  &mut ret as *mut u64 as *mut c_void, 8, None);
         // println!("b:{:?} rbuffer{:?} ",b, ret);
